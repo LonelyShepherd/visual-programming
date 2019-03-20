bb3f201c12290442864df732ce8f99adeab2142d
Nikola Stojanovikj <nikolastojanovic.hj@hotmail.com>
2019-03-21 00:59:07 +0100

    First lab exercise

---
+++ Lab1/Lab12/Lab12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab12
+{
+  class Program
+  {
+    static double findAvg(List<int> numbers)
+    {
+      return numbers.Average();
+    }
+
+    static void findMax(List<int> numbers, out int number)
+    {
+      number = numbers.Max();
+    }
+
+    static void findMin(List<int> numbers, out int number)
+    {
+      number = numbers.Min();
+    }
+
+    static void Main(string[] args)
+    {
+      // Console.WriteLine("How many numbers you want to enter?");
+      Console.WriteLine("How many random numbers you want to be generated?");
+
+      int n = Convert.ToInt32(Console.ReadLine());
+
+      List<int> numbers = new List<int>();
+      Random random = new Random();
+
+      for (int i = 0; i < n; i++)
+      {
+        //numbers.Add(Convert.ToInt32(Console.ReadLine()));
+
+        var number = random.Next(1, 1000);
+        Console.Write("{0} ", number);
+        numbers.Add(number);
+      }
+
+      var avg = findAvg(numbers);
+      int max, min;
+
+      findMax(numbers, out max);
+      findMin(numbers, out min);
+
+      Console.WriteLine();
+      Console.WriteLine("The average value from the entered numbers is: {0}.", avg /* numbers.Average() */);
+      Console.WriteLine("The max value from the entered numbers is: {0}.", max /* numbers.Max() */);
+      Console.WriteLine("The min value from the entered numbers is: {0}.", min /*numbers.Min() */);
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Lab1/Lab14/Lab14/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab14
+{
+  class Program
+  {
+    enum PasswordStrength
+    {
+      easy = 1,
+      normal = 2,
+      hard = 3
+    }
+
+    static Random random = new Random();
+
+    static string generatePassword(PasswordStrength passwordStrength)
+    {
+      const string charsForEasy = "abcdefghijklmnopqrstuvwxyz";
+      const string charsForNormal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const string charsForHard = charsForEasy + charsForNormal + "!@#$%^&*()_+/\\|";
+
+      switch (passwordStrength)
+      {
+        case PasswordStrength.easy:
+          return new string(Enumerable.Repeat(charsForEasy, 6)
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+        case PasswordStrength.normal:
+          return new string(Enumerable.Repeat(charsForNormal, random.Next(6, 10))
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+        case PasswordStrength.hard:
+        default:
+          return new string(Enumerable.Repeat(charsForHard, random.Next(10, 30))
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      string myString = "This is a test.";
+      char[] separator = { ' ' };
+      string[] myWords;
+
+      myWords = myString.Split(separator);
+
+      foreach (string word in myWords)
+      {
+        Console.WriteLine("{0}", word);
+      }
+
+      Console.WriteLine();
+      Console.WriteLine("Enter 3 passwords with different strength (easy, normal and hard):");
+      var enteredPasswords = Console.ReadLine().Split(new char[] { ' ' });
+
+      List<string> generatedPasswords = new List<string>();
+
+      int i = 0;
+
+      foreach (PasswordStrength strength in (PasswordStrength[]) Enum.GetValues(typeof(PasswordStrength)))
+      {
+        var generated = generatePassword(strength);
+        var entered = enteredPasswords[i];
+
+        Console.WriteLine("Your password {0} and the generated password {1} does {2}. ({3})", 
+          enteredPasswords[i], 
+          generated,
+          entered.Equals(generated) ? "match" : "not match",
+          Convert.ToString(strength)
+          );
+
+        i++;
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
